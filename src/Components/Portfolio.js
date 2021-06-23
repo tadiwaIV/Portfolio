12dcbccf5dc87837ef3a9838d4b5260ab72ec98f
tadiwaIV <tadiwachivhere@gmail.com>
2021-06-23 13:08:41 +0200

    Updated Projects
    Added a new completed project to my portfolio.

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import {Container, Row, Col, Button} from 'react-bootstrap';
 import Zmage from "react-zmage";
 import Fade from "react-reveal";
 
@@ -9,32 +10,37 @@ class Portfolio extends Component {
         return (
             <section id="portfolio">
         <Fade left duration={1000} distance="40px">
-          <div className="row">
-            <div className="twelve columns collapsed">
-              <h1>Check Out Some of My Projects</h1>
-
-              <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
-                <div class="columns portfolio-item">
-                  <div class="item-wrap">
-                    <img src="images/portfolio/NikeConcept.jpg" alt= "Nike Concept Page" />
-                    <span class="title" >Nike Concept Page</span>
-                  </div>
-                </div>
-                <div class="columns portfolio-item">
-                  <div class="item-wrap">
-                    <img src="images/portfolio/GigaGames.jpg" alt="Giga Games" />
-                    <span class="title" >Giga Games</span>
-                  </div>
-                </div>
-                <div class="columns portfolio-item">
-                  <div class="item-wrap">
-                    <img src="images/portfolio/OldPortfolio.jpg" alt="Old Portfolio" />
-                    <span class="title" >Old Portfolio</span>
-                  </div>
-                </div>                  
+          <Container>
+            <Row>
+              <div className="twelve columns collapsed">
+                <h1>Check Out Some of My Projects</h1>
               </div>
-            </div>
-          </div>
+            </Row>
+            <Row>
+              <div className="twelve columns collapsed">
+                <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+                  <div class="columns portfolio-item">
+                    <div class="item-wrap">
+                      <img src="images/portfolio/NikeConcept.jpg" alt= "Nike Concept Page" />
+                      <span class="title" >Nike Concept Page</span>
+                    </div>
+                  </div>
+                  <div class="columns portfolio-item">
+                    <div class="item-wrap">
+                      <img src="images/portfolio/GigaGames.jpg" alt="Giga Games" />
+                      <span class="title" >Giga Games</span>
+                    </div>
+                  </div>
+                  <div class="columns portfolio-item">
+                    <div class="item-wrap">
+                      <img src="images/portfolio/VibeCheck.jpg" alt="Old Portfolio" />
+                      <span class="title">Vibe Check</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </Row>
+          </Container>
         </Fade>
       </section>
         );
