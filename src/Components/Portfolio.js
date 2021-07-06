3664a2bb0c655dd9d803b2f75e891d8142c16a47
tadiwaIV <tadiwachivhere@gmail.com>
2021-07-06 17:06:27 +0200

    Update Portfolio.js

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -19,25 +19,33 @@ class Portfolio extends Component {
             <Row>
               <Col sm={12}>
                 <div class="item-wrap">
-                  <img src="images/portfolio/NikeConcept.jpg" alt= "Nike Concept Page" />
+                  <a href="https://github.com/tadiwaIV/Nike-Website">
+                    <img src="images/portfolio/NikeConcept.jpg" alt= "Nike Concept Page" />
+                  </a>
                   <span class="title" >Nike Concept Page</span>
                 </div>
               </Col>
               <Col sm={12}>
                 <div class="item-wrap">
+                <a href="https://github.com/tadiwaIV/GigaGames">
                   <img src="images/portfolio/GigaGames.jpg" alt="Giga Games" />
+                </a>
                   <span class="title" >Giga Games</span>
                 </div>
                 </Col>
               <Col sm={12}>
                 <div class="item-wrap">
+                <a href="https://github.com/tadiwaIV/vibe-check">
                   <img src="images/portfolio/VibeCheck.jpg" alt="VibeCheck" />
+                </a>
                   <span class="title">Vibe Check</span>
                 </div>
               </Col>
               <Col sm={12}>
                 <div class="item-wrap">
+                <a href="https://github.com/tadiwaIV/survey-form">
                   <img src="images/portfolio/SurveyForm.jpg" alt="Survey Form" />
+                </a>
                   <span class="title">Survey Form</span>
                 </div>
               </Col>
@@ -47,25 +55,33 @@ class Portfolio extends Component {
               <Row>
                 <Col sm={12}>
                   <div class="item-wrap">
+                  <a href="https://github.com/tadiwaIV/Door-2-Door">
                     <img src="images/portfolio/Door2Door.jpg" alt="Door2Door" />
+                  </a>
                     <span class="title">Door2Door</span>
                   </div>
                 </Col>
                 <Col sm={12}>
                   <div class="item-wrap">
+                  <a href="https://github.com/tadiwaIV/quote-generator">
                     <img src="images/portfolio/QuoteGenerator.jpg" alt="Quote Generator" />
+                  </a>
                     <span class="title">Quote Generator</span>
                   </div>
                 </Col>
                 <Col sm={12}>
                   <div class="item-wrap">
+                  <a href="https://github.com/tadiwaIV/tech-doc">
                     <img src="images/portfolio/TechDoc.jpg" alt="Technical Documentation" />
+                  </a>
                     <span class="title">Technical Documentation</span>
                   </div>
                 </Col>
                 <Col sm={12}>
                   <div class="item-wrap">
+                  <a href="https://github.com/tadiwaIV/tribute-page">
                     <img src="images/portfolio/TributePage.jpg" alt="Tribute Page" />
+                  </a>
                     <span class="title">Tribute Page</span>
                   </div>
                 </Col>
